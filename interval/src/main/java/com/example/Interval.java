6e76f6408457783bdb0d0d01837556e69f8b1971
Mario Blanco <mario10_bd@hotmail.com>
2024-04-06 00:17:47 +0200

    green, refactor

--- interval/src/main/java/com/example/Interval.java
+++ interval/src/main/java/com/example/Interval.java
@@ -2,18 +2,27 @@ package com.example;
 
 public class Interval {
     private double lowerBound;
+    private boolean includeLowerBound;
     private double upperBound;
     private boolean includeUpperBound;
 
     public Interval(double lowerBound, boolean includeLowerBound, double upperBound, boolean includeUpperBound) {
         this.lowerBound = lowerBound;
+        this.includeLowerBound = includeLowerBound;
         this.upperBound = upperBound;
         this.includeUpperBound = includeUpperBound;
     }
 
     public boolean include(double d) {
-        if (includeUpperBound)
-            return lowerBound <= d && d <= upperBound;
-        return lowerBound <= d && d < upperBound;
+        if (d < lowerBound || d > upperBound) {
+            return false;
+        }
+        if (d == lowerBound) {
+            return includeLowerBound;
+        }
+        if (d == upperBound) {
+            return includeUpperBound;
+        }
+        return true;
     }
 }
