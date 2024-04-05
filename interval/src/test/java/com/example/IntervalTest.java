c51353ee80ab750f7a977d790750fb30b62c1289
Mario Blanco <mario10_bd@hotmail.com>
2024-04-06 00:10:03 +0200

    red

--- interval/src/test/java/com/example/IntervalTest.java
+++ interval/src/test/java/com/example/IntervalTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -8,5 +9,10 @@ public class IntervalTest {
     @Test
     public void givenIntervalwhenIncludeWithIncludedValueThenTrue(){
       assertTrue(new Interval(-1.7,5555.0).include(0.0));
+    }
+
+    @Test
+    public void givenIntervalwhenIncludeWithExcludedValueThenFalse(){
+      assertFalse(new Interval(-1.7,5555.0).include(5555.1));
     }
 }
