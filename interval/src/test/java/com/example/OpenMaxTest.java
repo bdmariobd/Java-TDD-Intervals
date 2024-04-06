786a86660b78fd4a85b5df2aa810109c830d138e
Mario Blanco <mario10_bd@hotmail.com>
2024-04-06 09:58:55 +0200

    red

--- interval/src/test/java/com/example/OpenMaxTest.java
+++ interval/src/test/java/com/example/OpenMaxTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,6 +10,11 @@ public class OpenMaxTest{
     @Test
     public void givenOpenMaxWhenGreaterThanThenTrue() {
         assertTrue(new OpenMax(1.0).greaterThan(0.0));
+    }
+
+    @Test
+    public void givenOpenMaxWhenNotGreaterThanThenFalse() {
+        assertFalse(new OpenMax(0).greaterThan(1.0));
     }
 
 }
