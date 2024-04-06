9fb57d95dec833c3d1ca102beb09dff4d05e1fb0
Mario Blanco <mario10_bd@hotmail.com>
2024-04-06 09:12:54 +0200

    red

--- interval/src/test/java/com/example/MaxTest.java
+++ interval/src/test/java/com/example/MaxTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,5 +10,10 @@ public class MaxTest {
     @Test
     public void givenIntervalwhenIncludeWithIncludedValueThenTrue(){
       assertTrue(new Max(4, true).greaterThan(0.0));
+    }
+
+    @Test
+    public void givenIntervalwhenIncludeWithExcludedValueThenFalse(){
+      assertFalse(new Max(-4, true).greaterThan(0.0));
     }
 }
