73377ea7e485a70bf6df63ef8e0248817f8caf2f
Mario Blanco <mario10_bd@hotmail.com>
2024-04-06 09:14:45 +0200

    green

--- interval/src/main/java/com/example/Max.java
+++ interval/src/main/java/com/example/Max.java
@@ -1,14 +1,17 @@
 package com.example;
 
 public class Max {
-    
     double number;
-
+    boolean include;
     public Max(double number, boolean include) {
         this.number = number;
+        this.include = include;
     }
 
     public boolean greaterThan(double valueToCompare) {
-        return this.number >= valueToCompare;
+        if (include) {
+            return valueToCompare <= number;
+        }
+        return valueToCompare < number;
     }
 }
